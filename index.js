30078a4271e8d5cb0a2ef05539c74aff303d9565
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2024-01-05 15:28:57 -0300

    Configurando e exibindo html com ejs

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+app.set('view engine', 'ejs');//dizendo ao express para usar o ejs como motor de engine ou template
+
+app.get("/", function(req, res){
+    res.render("home"); //o metódo render irá converter os comandos html e apresentar o desenho da tela para o usuário passando como parâmetro o arquivo da view
+});
+
+app.get("/perfil/usuario", function(req, res){
+    res.render("principal/perfil");//só será necessário especificar o caminho do arquivo quando este estiver dentro de uma pasta dentro da pasta views
+});
+
+app.listen(3000,()=>{
+    console.log("servidor rodando");
+});
