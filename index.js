d919d5d6d043ede969c533ad81bdd165c94fa445
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2024-01-09 18:14:58 -0300

    Adicionando css e javascript do bootstrap no projeto

--- index.js
+++ index.js
@@ -3,23 +3,9 @@ const app = express();
 app.set('view engine', 'ejs');//dizendo ao express para usar o ejs como motor de engine ou template
 app.use(express.static('public'));//dizendo ao express para usar arquivos estáticos na pasta public
 
-app.get("/:nome/:lang", function(req, res){
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
-    var produtos = [
-        {nome: "Coca-cola", preco: 10.00},
-        {nome: "Leite", preco: 5.00}
-    ]
-
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Guia do programador",
-        msg: exibirMsg,
-        produtos: produtos,
-    }); //o metódo render irá converter os comandos html e apresentar o desenho da tela para o usuário passando como parâmetro o arquivo da view
-});
+app.get("/", function(req, res){
+    res.render("index");
+}); //o metódo render irá converter os comandos html e apresentar o desenho da tela para o usuário passando como parâmetro o arquivo da view
 
 app.get("/perfil/usuario", function(req, res){
     res.render("principal/perfil");//só será necessário especificar o caminho do arquivo quando este estiver dentro de uma pasta dentro da pasta views
