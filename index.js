09b1c10d9b30bf79057c27b84b24d3004d9ba33e
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2024-01-10 22:38:41 -0300

    Criando formulario de perguntas

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.get("/", function(req, res){
     res.render("index");
 }); //o metódo render irá converter os comandos html e apresentar o desenho da tela para o usuário passando como parâmetro o arquivo da view
 
-app.get("/perfil/usuario", function(req, res){
-    res.render("principal/perfil");//só será necessário especificar o caminho do arquivo quando este estiver dentro de uma pasta dentro da pasta views
+app.get("/perguntar", function(req, res){
+    res.render("perguntar");
 });
 
 app.listen(3000,()=>{
