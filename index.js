60e38d7428f61d07d6b8092b559ab8ae83db68ad
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2024-01-12 19:12:33 -0300

    Envio de formulario para o nodejs

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const app = express();
 app.set('view engine', 'ejs');//dizendo ao express para usar o ejs como motor de engine ou template
 app.use(express.static('public'));//dizendo ao express para usar arquivos estáticos na pasta public
 
-app.get("/", function(req, res){
+app.get("/", (req, res)=>{
     res.render("index");
 }); //o metódo render irá converter os comandos html e apresentar o desenho da tela para o usuário passando como parâmetro o arquivo da view
 
-app.get("/perguntar", function(req, res){
+app.get("/perguntar", (req, res)=>{
     res.render("perguntar");
+});
+
+app.post("/salvarPergunta", (req, res)=>{ //passando a rota do tipo post
+    res.send("Formulario enviado com sucesso");
 });
 
 app.listen(3000,()=>{
