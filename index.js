faebb23185be6e535041572fa9880278268f7704
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2024-01-13 11:36:32 -0300

    Adicionando o modulo body-parser e acessando dados do formulario

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");//incluindo o modeulo bodyparser que traduz os dados enviados pelo usuário em comandos javascript e permite a utilização destes dentro do projeto
+
 app.set('view engine', 'ejs');//dizendo ao express para usar o ejs como motor de engine ou template
 app.use(express.static('public'));//dizendo ao express para usar arquivos estáticos na pasta public
+
+app.use(bodyParser.urlencoded({extended: false}));//decodifica os dados enviados pelo formulário em comandos javascript
+//app.use(bodyParser.json);//permite a leitura de dados de formulário via json, bem usado para API
 
 app.get("/", (req, res)=>{
     res.render("index");
 }); //o metódo render irá converter os comandos html e apresentar o desenho da tela para o usuário passando como parâmetro o arquivo da view
 
+//Rotas
 app.get("/perguntar", (req, res)=>{
     res.render("perguntar");
 });
 
 app.post("/salvarPergunta", (req, res)=>{ //passando a rota do tipo post
-    res.send("Formulario enviado com sucesso");
+    var titulo = req.body.titulo;//acessando o titulo pelo objeto 'body' do modulo body-parser
+    var descricao = req.body.descricao;//acessando a descrição pelo objeto 'body' do modulo body-parser
+    res.send("Formulario enviado com sucesso" + " " + `Titulo: ${titulo} e descricao: ${descricao}`);
 });
 
 app.listen(3000,()=>{
