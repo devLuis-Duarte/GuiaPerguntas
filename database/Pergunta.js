e38b31cb58d72c7a5fc5ba39f8dd4907d661681e
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2024-01-18 11:09:32 -0300

    Salvando perguntas no banco de dados e alterando nome do atributo description para descricao na tabela perguntas

--- database/Pergunta.js
+++ database/Pergunta.js
@@ -7,10 +7,12 @@ const Pergunta = connection.define('perguntas', {
         type: Sequelize.STRING, //tipo do dado, nesse caso, STRING que é para textos curtos
         allowNull: false, //allowNull: false, impedirá de receber valores nulos
     },
-    description:{
+    descricao:{
         type: Sequelize.TEXT, //tipo do dado, nesse caso, TEXT que é para textos longos
         allowNull: false, 
     }
 });
 
 Pergunta.sync({force: false}).then(()=>{}); //sincroniza com o banco de dados e o force: false impedirá de criar uma tabela pergunta, caso ela já exista no banco. o then é o que ele fará após criar a tabela
+
+module.exports = Pergunta;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");//incluindo o módulo bodyparser que traduz os dados enviados pelo usuário em comandos javascript e permite a utilização destes dentro do projeto
 const connection = require("./database/database");//importando o arquivo correspondente à conexão do banco
-const perguntaModel = require("./database/pergunta");//importando o arquivo correspondente à model Pergunta do banco
+const Pergunta = require("./database/pergunta");//importando o arquivo correspondente à model Pergunta do banco
 //Database
 
 connection
@@ -14,7 +14,6 @@ connection
 })
 app.set('view engine', 'ejs');//dizendo ao express para usar o ejs como motor de engine ou template
 app.use(express.static('public'));//dizendo ao express para usar arquivos estáticos na pasta public
-
 app.use(bodyParser.urlencoded({extended: false}));//decodifica os dados enviados pelo formulário em comandos javascript
 //app.use(bodyParser.json);//permite a leitura de dados de formulário via json, bem usado para API
 
@@ -30,7 +29,16 @@ app.get("/perguntar", (req, res)=>{
 app.post("/salvarPergunta", (req, res)=>{ //passando a rota do tipo post
     var titulo = req.body.titulo;//acessando o titulo pelo objeto 'body' do modulo body-parser
     var descricao = req.body.descricao;//acessando a descrição pelo objeto 'body' do modulo body-parser
-    res.send("Formulario enviado com sucesso" + " " + `Titulo: ${titulo} e descricao: ${descricao}`);
+    
+    //semelhante ao comando INSERT INTO table_name VALUES()
+    Pergunta.create({ //chamando o metódo create que vai inserir os dados na tabela da model Pergunta
+        //passando os valores do formulário para a tabela perguntas do banco
+        titulo: titulo,
+        descricao: descricao,
+    }).then(()=>{
+        res.redirect("/"); //redirecionando o usuário para a página inicial
+    });
+    
 });
 
 app.listen(3000,()=>{
